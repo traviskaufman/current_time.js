e6b46ca176a8f1453e2a1e82ca5a3fa24e98a59b
Travis Kaufman <travis.kaufman@gmail.com>
2013-04-02 17:16:11 -0400

    Add JSHint/Grunt, and that will be ALL the time I'm spending on automation -_-

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+require('shelljs/global');
+
+/**
+ * Grunt Configuration.
+ * @param {Object} grunt Grunt Object.
+ */
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      all: ['*.js', './test/*.js', 'Gruntfile.js'],
+      jshintrc: grunt.file.read(__dirname + '/jshintrc')
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('test', 'Run mocha tests', function() {
+    var result = exec(__dirname +
+         '/node_modules/.bin/mocha --require should --reporter spec');
+
+    if (result.code !== 0) {
+      grunt.fail.warn("Mocha tests did not pass");
+    }
+  });
+
+  grunt.registerTask('default', ['jshint', 'test']);
+};
