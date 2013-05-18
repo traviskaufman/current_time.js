0da99e6c139a8d9179d425b52d0cd06fc9429264
Travis Kaufman <travis.kaufman@gmail.com>
2013-05-18 18:58:55 -0400

    write tests to ensure user-defined update callbacks are being honored and do some refactoring

--- test/current_time.spec.js
+++ test/current_time.spec.js
@@ -1,28 +1,28 @@
 /**
  * Unit tests for CurrentTime module.
  */
+var sinon = require('sinon');
 
 describe('CurrentTime.js', function() {
   describe('Core Funtionality', function() {
     var CurrentTime = require('../current_time');
     var dateObj = new Date("Mon, 1 Apr 2013 13:21:46");
+    var _update = function() { CurrentTime.update(dateObj); };
 
-    before(function() {
-      CurrentTime.update(dateObj);
-    });
+    var assertValidTimeComponent = function(timeComponent, expected) {
+      timeComponent.raw.should.equal(expected);
+
+      ((timeComponent.tens * 10) + timeComponent.ones)
+        .should.equal(expected);
+    };
+
+    before(_update);
 
     describe('Date parsing', function() {
       var curTime;
       before(function() {
         curTime = CurrentTime.get();
       });
-
-      var assertValidTimeComponent = function(timeComponent, expected) {
-        timeComponent.raw.should.equal(expected);
-
-        ((timeComponent.tens * 10) + timeComponent.ones)
-          .should.equal(expected);
-      };
 
       it('parses hours', function() {
         assertValidTimeComponent(curTime.hours, 13);
@@ -58,6 +58,43 @@ describe('CurrentTime.js', function() {
         str.should.equal("13 %y");
       });
 
+    });
+
+    describe('Extensibility', function() {
+      describe('Custom Update Function', function() {
+        var _updateFn;
+
+        beforeEach(function() {
+          _updateFn = sinon.spy();
+          CurrentTime.onUpdate(_updateFn);
+          _update();
+        });
+
+        it('allows for a callback to be invoked on updates', function() {
+          _updateFn.called.should.equal(true);
+        });
+
+        it('passes a time object as the first callback arg', function() {
+          var timeArg = _updateFn.getCall(0).args[0];
+
+          assertValidTimeComponent(timeArg.hours, 13);
+          assertValidTimeComponent(timeArg.minutes, 21);
+          assertValidTimeComponent(timeArg.seconds, 46);
+          timeArg.meridian.should.equal("pm");
+        });
+
+        it('passes the raw date object as the 2nd callback arg', function() {
+          _updateFn.getCall(0).args[1].should.eql(dateObj);
+        });
+
+        it('ensures CurrentTime is always the receiver', function() {
+          _updateFn.calledOn(CurrentTime).should.equal(true);
+        });
+      });
+
+      describe('Client-defined time symbols', function() {
+        // TODO: addSymbol(s)
+      });
     });
 
   });
